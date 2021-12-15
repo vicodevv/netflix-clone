9b1d9b962ea813d64deb20c4ec457c9b8ca82ea6
Victor Omoniyi <vomoniyi2001@gmail.com>
2021-12-15 13:58:33 +0100

    video

---
+++ src/components/listitem/ListItem.jsx
@@ -0,0 +1,37 @@
+import { PlayArrow, ThumbDownAltOutlined, ThumbUpAltOutlined, Add } from "@material-ui/icons"
+import { useState } from "react"
+import "./listitem.scss"
+
+export default function ListItem({index}) {
+    const[isHovered, setIsHovered] = useState(false)
+    const trailer = "https://www.imdb.com/video/vi673889049?playlistId=tt5093026"
+    return (
+        <div className="listItem" 
+        onMouseEnter={() => setIsHovered(true)} 
+        onMouseLeave={() => setIsHovered(false)} 
+        style={{left:isHovered && index * 225 -50 + index * 2.5}}>
+           <img src="https://occ-0-4616-2706.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABe4MOeHcKDegnaMHGCqTNKvf2NO1sJviVS4NnNkIYoiNV8ERxGvYeH7jM6wjzOL7q0mNbIxyeo7w6NqsSsK-2p9UbY0iaKO7uEAMlCAzYyTCArnGsO7yXW7Gn-8YKG30KjqtUZR38aiob2-ngg4tVK_Oh0Q.webp?r=5dd" alt="" />
+           {isHovered && (
+           <>
+           <video src={trailer} autoPlay={true} loop></video>
+        <div className="itemInfo">
+            <div className="icons">
+                <PlayArrow/>
+                <Add/>
+                <ThumbUpAltOutlined/>
+                <ThumbDownAltOutlined/>
+            </div>
+            <div className="itemInfoTop">
+                <span>2h 12m</span>
+                <span className="limit">18+</span>
+            </div>
+            <div className="desc">
+            Framed and condemned to a penal colony in French Guiana, a safecracker bonds with a counterfeiter and fellow prisoner in a dangerous quest for freedom.
+            </div>
+            <div className="genre">Action</div>
+        </div> </>
+        )}
+           
+        </div>
+    );
+}
