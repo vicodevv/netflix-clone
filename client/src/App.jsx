0b6909872439a5d13c38d6cfa49690e0fdc001c3
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-01-31 21:57:06 +0100

    route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,21 @@
 import  Home  from "./pages/home/Home";
 import "./app.scss"; 
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 const App = () => {
-  return <Home/>
+  return (
+    <Router>
+      <Routes>
+  <Route path="/" element={<Home />}>
+    <Home />
+  </Route>
+  <Route path="/movies" element={<Home />}>
+    <Home />
+  </Route>
+      </Routes>
+    </Router>
+  
+  );
 };
 
 export default App;
