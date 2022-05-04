0367d9254caa0df7a4b695e1cf1317f920ae2f13
Victor Omoniyi <vomoniyi2001@gmail.com>
2022-05-04 15:20:28 +0100

    route fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,8 @@
-import  Home  from "./pages/home/Home";
+import Register from "./pages/register/Register";
 import "./app.scss"; 
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 
 const App = () => {
-  return (
-    <Router>
-      <Routes>
-  <Route path="/" element={<Home />}>
-    <Home />
-  </Route> 
-  <Route path="/movies" element={<Home />}>
-    <Home />
-  </Route>
-      </Routes>
-    </Router>
-  
-  );
+  return <Register/>
 };
 
 export default App;
